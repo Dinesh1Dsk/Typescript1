35dc7554493e2f799b2af1c882a50c746c6182b0
Dinesh1Dsk <espoir.mdu@gmail.com>
2023-04-10 06:46:48 +0530

    folder

---
+++ New folder/index.ts
@@ -0,0 +1,11 @@
+let message: string = 'Hello, World!';
+
+let num:number = 20;
+let num2:number=30;
+console.log(message+(num+num2));
+let head=document.createElement('h1')
+head.textContent=message;
+head.setAttribute('id','dk');
+
+
+document.body.appendChild(head);
